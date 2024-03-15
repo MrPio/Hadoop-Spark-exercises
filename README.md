251b1c51d342491829f8010fa136a9ffe9928999
MrPio <valeriomorelli50@gmail.com>
2024-03-15 18:58:53 +0100

    Repo cleaned, new Readme, stable code

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Esercitazioni-BDD-ML
-Repository contenente le attività svolte per le esercitazioni del corso "Big Data Analytics and Machine Learning" UNIVPM anno 2023-2024
+# Assignment-BDD-ML
+Repository containing the activities carried out for the "Big Data Analytics and Machine Learning" UNIVPM course exercises year 2023-2024
+
+## Assignment 1 (Spark)
+This task revolves around the <a href="https://www.databricks.com/it/glossary/pyspark">PySpark</a> framework and consists of implementing 3 queries and executing them in as little time as possible on a large dataset.
+
+<a href="assignment_1/assignment.pdf">📘 Assignment PDF </a>
+
+<a href="https://stackoverflow.com/questions/78165804/pyspark-how-to-read-multiple-csv-files-with-different-column-positions-most-eff">❓ Encountered problem</a>
+
+<a href="assignment_1/main.py">✅ Solution</a>
--- assignment_1/main.py
+++ assignment_1/main.py
@@ -6,8 +6,6 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import split, col, count, size, format_string
 from pyspark.sql.types import FloatType
 
-# TODO: read_csv should be invoked only once (maybe using cache() method?)
-
 sc = SparkContext.getOrCreate()
 spark = SparkSession(sc)
 i_time = time()
@@ -38,20 +36,6 @@ def write_to_file(filename, content):
         file.write(content)
 
 
-def op1_slow():
-    """
-    Op1: print out the number of measurements taken per year for each station (sorted by year and station)
-    Note: this is elegant but slow as it separately invokes spark for each file without exploiting its capabilities
-    """
-    scan_directory()
-    results = []
-    for year in years:
-        for ws in weather_stations:
-            if year in ws.years:
-                results.append('{},{},{}'.format(year, ws.name, ws.get_dataframe(year).count()))
-    write_to_file('o1.txt', '\n'.join(results))
-
-
 def read_csv(year='*', station='*'):
     return spark.read.format('csv') \
         .option('header', 'true') \
@@ -61,13 +45,13 @@ def read_csv(year='*', station='*'):
         .withColumn('station', split(col('_metadata.file_name'), '.csv')[0])
 
 
-def op1():
+def op1(df):
     """
     Op1: print out the number of measurements taken per year for each station (sorted by year and station)
     Note: this is fast as it exploits on spark dataframe transformations
     """
-    df = read_all_datasets() \
-        .select(['_metadata.file_path', 'year', 'station']) \
+    result_df = df \
+        .select(['year', 'station']) \
         .groupBy('year', 'station') \
         .agg(count('*').alias('num_measures')) \
         .orderBy('year', 'station') \
@@ -76,15 +60,15 @@ def op1():
     # if os.path.exists(out_file_path):
     #     os.remove(out_file_path)
     # df.write.format("text").option("header", "false").mode("append").save(out_file_path)
-    write_to_file('o1.txt', '\n'.join([row.result for row in df.rdd.collect()]))
+    write_to_file('o1.txt', '\n'.join([row.result for row in result_df.rdd.collect()]))
 
 
-def op2():
+def op2(df):
     """
     Op2: print the top 10 temperatures (TMP) with the highest number of occurrences and count recorded
     in the highlighted area (sorted by number of occurrences and temperature)
     """
-    df = read_all_datasets() \
+    result_df = df \
         .withColumn("LATITUDE", col('LATITUDE').cast(FloatType())) \
         .withColumn("LONGITUDE", col('LONGITUDE').cast(FloatType())) \
         .select(['LATITUDE', 'LONGITUDE', 'TMP']) \
@@ -94,60 +78,47 @@ def op2():
         .agg(count('*').alias('num_occurrences')) \
         .orderBy(col("num_occurrences").desc(), col("TMP").asc())
     results = []
-    for row in df.rdd.take(10):
+    for row in result_df.rdd.take(10):
         tmp = float(str(row.TMP).split(',')[0]) / 10
         results.append('[(60,-135);(30,-90)],{},{})'.format(tmp, row.num_occurrences))
     write_to_file('o2.txt', '\n'.join(results))
 
 
-def op3():
+def op3(df):
     """
     Op3: print out the station with the speed in knots and its count
     (sorted by count, speed and station)
     """
-    df = read_all_datasets() \
+    result_df = df \
         .select(['station', 'WND']) \
         .withColumn('WDN_knots', split(col('WND'), ',')[1]) \
         .groupBy('station', 'WDN_knots') \
         .agg(count('*').alias('num_occurrences')) \
         .orderBy(col("num_occurrences").desc(), col("WDN_knots").asc(), col("station").asc())
-    first_row = df.first()
+    first_row = result_df.first()
     write_to_file('o3.txt', '{},{},{}'.format(first_row.station, first_row.WDN_knots, first_row.num_occurrences))
 
 
+COLUMNS_HAVE_SAME_INDEX = True
 if __name__ == '__main__':
     # Read all csv files
-    dfs = []
-    for year in list_file_names(dataset_path):
-        for station in list_file_names('{}/{}'.format(dataset_path, year)):
-            dfs.append(read_csv(year, station))
-    union_df = dfs[0]
-    for df in dfs[1:]:
-        union_df = union_df.unionByName(df, allowMissingColumns=True)
+    if COLUMNS_HAVE_SAME_INDEX:
+        union_df = read_csv()
+    else:
+        dfs = []
+        for year in list_file_names(dataset_path):
+            for station in list_file_names('{}/{}'.format(dataset_path, year)):
+                dfs.append(read_csv(year, station.split('.csv')[0]))
+        # union_df = reduce(lambda x, y: x.unionByName(y, allowMissingColumns=True), dfs)
+        union_df = dfs[0]
+        for df in dfs[1:]:
+            union_df = union_df.unionByName(df, allowMissingColumns=True)
+    # union_df.cache()
 
-    # Step 2: Read CSV Files with Inferred Schema
-    folder_path = "/path/to/your/csv/folder"
-    csv_files = spark.read.option("header", "true").option("inferSchema", "true").csv(folder_path)
-    common_columns = set(csv_files.columns)
-    for file in csv_files.collect()[1:]:
-        common_columns = common_columns.intersection(set(file.columns))
-    dfs = []
-    for file in csv_files.collect():
-        reordered_df = file.select(*common_columns)
-        dfs.append(reordered_df)
-    final_df = dfs[0].union(dfs[1:])
-    final_df.show()
-
-    # TODO: The following bypasses the lack of the `allowMissingColumns' parameter in older Spark versions
-    #  by creating a dummy col to make the number even.
-    # cols = [len(df.columns) for df in df_list]
-    # max_cols = max(cols)
-    # df_list = [df.select(
-    #     *[col(c) for c in df.columns] + [lit(None).alias("col_{}".format(i + j)) for i in range(len(df.columns), max_cols)]
-    # ) for j, df in enumerate(df_list)]
-    # df_final = reduce(lambda x, y: x.unionByName(y), df_list)
-
-    op1()
-    op2()
-    op3()
-    print('Operations terminated in {} s.'.format(time() - i_time))
+    print('Dataframe loaded (lazy) in {} s.'.format(time() - i_time))
+    op1(union_df)
+    print('Op 1 terminated in {} s.'.format(time() - i_time))
+    op2(union_df)
+    print('Op 2 terminated in {} s.'.format(time() - i_time))
+    op3(union_df)
+    print('All operations have terminated in {} s.'.format(time() - i_time))
