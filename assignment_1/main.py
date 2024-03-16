d98757712c81fe1639c17c5c283e3ab65f624762
MrPio <valeriomorelli50@gmail.com>
2024-03-16 14:20:01 +0100

    Performance improved as explained in https://stackoverflow.com/a/78171905/19815002

--- assignment_1/main.py
+++ assignment_1/main.py
@@ -1,4 +1,5 @@
 import os
+from functools import reduce
 from time import time
 
 from pyspark import SparkContext
@@ -9,21 +10,21 @@ from pyspark.sql.types import FloatType
 sc = SparkContext.getOrCreate()
 spark = SparkSession(sc)
 i_time = time()
-dataset_path = 'hdfs:///BigDataCSV'  # 'hdfs://192.168.104.45:9000/user/amircoli/BDA2324'
-output_path = '/home/user/results/5'  # '/home/amircoli/BDAchallenge2324/results/5'
+DATASET_PATH = 'hdfs:///BigDataCSV'  # 'hdfs://192.168.104.45:9000/user/amircoli/BDA2324'
+OUTPUT_PATH = '/home/user/results/5'  # '/home/amircoli/BDAchallenge2324/results/5'
+COLUMNS_OF_INTEREST = ['LATITUDE', 'LONGITUDE', 'WND', 'TMP']
+COLUMNS_HAVE_SAME_INDEX = False
+
 # Create the output path if it doesn't already exist
 try:
-    os.makedirs(output_path)
+    os.makedirs(OUTPUT_PATH)
 except OSError as e:
     pass
 
 
 def list_file_names(directory_path):
     """
-    List all files in a given directory
-    :param sc: the SparkContext
-    :param directory_path: the directory path in which the files are
-    :return: the list of file names in the directory
+    List all files in a given directory on the hdfs filesystem
     """
     file_status_objects = sc._gateway.jvm.org.apache.hadoop.fs.FileSystem.get(sc._jsc.hadoopConfiguration()).listStatus(
         sc._jvm.org.apache.hadoop.fs.Path(directory_path)
@@ -32,14 +33,39 @@ def list_file_names(directory_path):
 
 
 def write_to_file(filename, content):
-    with open('{}/{}'.format(output_path, filename), "w") as file:
+    with open('{}/{}'.format(OUTPUT_PATH, filename), "w") as file:
         file.write(content)
 
 
-def read_csv(year='*', station='*'):
+def read_headers():
+    """
+    Read headers from the CSV files and return them as a dict, where the key is a tuple of the indices
+    of the columns of interest, and the value is the list of file paths
+    Note: To minimize the number of unionByName operations, we only look at the indexes of the columns
+    of interest of the subsequent queries
+    """
+    headers = {}
+    for year in list_file_names(DATASET_PATH):
+        for station in list_file_names('{}/{}'.format(DATASET_PATH, year)):
+            path = '{}/{}/{}'.format(DATASET_PATH, year, station)
+            header = spark.read.option("header", "true").csv(path).columns
+            # Another way to go is with smart_open:
+            # header = smart_open.smart_open('{}/{}/{}'.format(dataset_path, year, station), 'r').readline().strip()
+            key = tuple(header.index(c) for c in COLUMNS_OF_INTEREST)
+            headers[key] = headers.get(key, []) + [(year, station)]
+    return headers
+
+
+def read_csv(csv_files=None):
+    """
+    Perform a spark read operation and use the _metadata column to create two columns,
+    one for the year and one for the station name
+    """
+    if csv_files is None:
+        csv_files = '{}/*/*.csv'.format(DATASET_PATH)
     return spark.read.format('csv') \
         .option('header', 'true') \
-        .load('{}/{}/{}.csv'.format(dataset_path, year, station)) \
+        .load(csv_files) \
         .withColumn('year', split(col('_metadata.file_path'), '/')) \
         .withColumn('year', col('year')[size('year') - 2]) \
         .withColumn('station', split(col('_metadata.file_name'), '.csv')[0])
@@ -99,20 +125,22 @@ def op3(df):
     write_to_file('o3.txt', '{},{},{}'.format(first_row.station, first_row.WDN_knots, first_row.num_occurrences))
 
 
-COLUMNS_HAVE_SAME_INDEX = True
 if __name__ == '__main__':
     # Read all csv files
     if COLUMNS_HAVE_SAME_INDEX:
         union_df = read_csv()
     else:
+        headers = read_headers()
+        print('CSV headers checked in {} s.'.format(time() - i_time))
         dfs = []
-        for year in list_file_names(dataset_path):
-            for station in list_file_names('{}/{}'.format(dataset_path, year)):
-                dfs.append(read_csv(year, station.split('.csv')[0]))
-        # union_df = reduce(lambda x, y: x.unionByName(y, allowMissingColumns=True), dfs)
-        union_df = dfs[0]
-        for df in dfs[1:]:
-            union_df = union_df.unionByName(df, allowMissingColumns=True)
+        for stations in headers.values():
+            files = ['{}/{}/{}'.format(DATASET_PATH, year, station) for year, station in stations]
+            dfs.append(read_csv(files))
+        # union_df = dfs[0]
+        # for df in dfs[1:]:
+        #     union_df = union_df.unionByName(df, allowMissingColumns=True)
+        # Alternatively, with the `reduce` spark function
+        union_df = reduce(lambda x, y: x.unionByName(y, allowMissingColumns=True), dfs)
     # union_df.cache()
 
     print('Dataframe loaded (lazy) in {} s.'.format(time() - i_time))
